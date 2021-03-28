347a07fb2f8680400181484b504ff818956aefb2
rafaraca <rafarac@gmail.com>
2021-03-28 15:46:20 -0300

    Primeiro Commit

---
+++ src/components/SnackBar.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-snackbar
+    v-model="show"
+  >
+    {{ text }}
+
+    <template v-slot:action="{ attrs }">
+      <v-btn
+        color="blue"
+        text
+        v-bind="attrs"
+        @click="closeSnackbar()"
+      >
+        Close
+      </v-btn>
+    </template>
+  </v-snackbar>
+</template>
+
+<script>
+export default {
+  props: {
+    show: {
+      type: Boolean,
+      default: false
+    },
+    text: {
+      type: String,
+      default: ""
+    }
+  },
+
+  watch: {
+    show(newValue){
+      if(newValue){
+        setTimeout(()=>{
+          this.closeSnackbar()
+        },3000);
+      }
+    }
+  },
+
+  methods: {
+    closeSnackbar(){
+      this.$emit('closeSnackbar')
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
